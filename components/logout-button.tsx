80eab678970057c1e8943d034c19aaf3b6b3d8e3
Nathan Adhitya <NathanAdhitya@users.noreply.github.com>
2025-12-17 08:13:37 +0700

    fix logout button state

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -1,20 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { logoutUser } from "@/lib/actions/user-actions";
-import { useState } from "react";
 
 export function LogoutButton() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onClick = async () => {
-    setIsLoading(true);
-    await logoutUser();
-    setIsLoading(false);
-  };
-
-  return (
-    <Button onClick={onClick} disabled={isLoading}>
-      {isLoading ? "Logging out..." : "Logout"}
-    </Button>
-  );
+  return <Button onClick={logoutUser}>Logout</Button>;
 }
